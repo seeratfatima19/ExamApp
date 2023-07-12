599116527e31148bf107e51a2873844eab3a19f2
seeratfatima19 <seeratfatima991@yahoo.com>
2023-07-12 22:32:12 +0500

    first commit

---
+++ app/src/main/java/com/example/skyroot/ExamFragment.java
@@ -0,0 +1,29 @@
+package com.example.skyroot;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ExamFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ExamFragment extends Fragment {
+
+    public ExamFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exam, container, false);
+    }
+}
